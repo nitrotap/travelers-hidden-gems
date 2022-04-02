e16357626e40dc4be16f4252ae4fb8ae9651c37f
Pat Lawler <patlaw777@icloud.com>
2022-04-02 12:26:20 -0500

    added singel-post route and linked from homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -37,6 +37,48 @@ router.get('/', (req, res) => {
 			res.status(500).json(err);
 		});
 });
+router.get('/post/:id', (req, res) => {
+  Post.findAll({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'title',
+      'contents',
+      'location',
+      'icon',
+      'user_id',
+      'created_at'
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'user_id', 'post_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+  .then(dbPostData => {
+    if(!dbPostData) {
+      res.status(404).json({message: 'post with that id was not found'})
+      return;
+    }
+    const post = dbPostData[0].get({ plain: true });
+    res.render('single-post', { req, post });
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
 router.get('/login', (req, res) => {
 	if (req.session.loggedIn) {
 		res.redirect('/');
