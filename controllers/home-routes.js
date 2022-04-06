7360449995a14bd68bb8addac0e5a1e4ead941f6
Pat Lawler <patlaw777@icloud.com>
2022-04-05 21:16:29 -0500

    updated styling to main and single post views

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,7 +3,6 @@ const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
-
 router.get('/', (req, res) => {
   Post.findAll({
     attributes: [
