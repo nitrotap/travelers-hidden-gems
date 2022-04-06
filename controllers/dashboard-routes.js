bac4217dcc79e9e2aea51713316ee95a326f792d
Colby Harris <harris.colby@gmail.com>
2022-04-06 18:48:28 -0500

    fixed the button issues and set up the bookmarks page, still need correct sequelize logic

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -98,4 +98,48 @@ router.get('/newpost', withAuth, (req, res) => {
         })
 });
 
+router.get('/bookmarks', withAuth, (req, res) => {
+  Post.findAll({
+    where: {
+      user_id: req.session.user_id
+    },
+    attributes: [
+      'id',
+      'title',
+      'contents',
+      'latitude',
+      'longitude',
+      'icon',
+      'user_id',
+      'created_at'
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      const posts = dbPostData.map(post => post.get({ plain: true }));
+      res.render('bookmarks', {
+        posts,
+        req,
+        loggedIn: req.session.loggedIn
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 module.exports = router;
