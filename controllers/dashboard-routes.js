ed1c1fc69de7bda98b9248117f13a899deb868c8
Colby Harris <harris.colby@gmail.com>
2022-04-06 19:37:11 -0500

    added bookmark page and functionality

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { Post, User, Comment } = require('../models');
+const { Post, User, Comment, Bookmark } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', withAuth, (req, res) => {
@@ -98,28 +98,27 @@ router.get('/newpost', withAuth, (req, res) => {
         })
 });
 
-router.get('/bookmarks', withAuth, (req, res) => {
-  Post.findAll({
+router.get('/bookmarks', (req, res) => {
+  Bookmark.findAll({
     where: {
       user_id: req.session.user_id
     },
     attributes: [
       'id',
-      'title',
-      'contents',
-      'latitude',
-      'longitude',
-      'icon',
       'user_id',
-      'created_at'
+      'post_id'
     ],
     include: [
       {
-        model: Comment,
-        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        model: Post,
+        attributes: ['id', 'title', 'contents', 'latitude', 'longitude', 'icon', 'user_id', 'created_at'],
         include: {
-          model: User,
-          attributes: ['username']
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
         }
       },
       {
@@ -129,9 +128,10 @@ router.get('/bookmarks', withAuth, (req, res) => {
     ]
   })
     .then(dbPostData => {
-      const posts = dbPostData.map(post => post.get({ plain: true }));
+      const bookmarks = dbPostData.map(post => post.get({ plain: true }));
+      // res.json(bookmarks)
       res.render('bookmarks', {
-        posts,
+        bookmarks,
         req,
         loggedIn: req.session.loggedIn
       });
