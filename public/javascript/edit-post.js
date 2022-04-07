1878885d7d0962307b3417f792c26243105674c9
Colby Harris <harris.colby@gmail.com>
2022-04-06 22:53:09 -0500

    fixed the edit post bug

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -2,6 +2,7 @@ async function editFormHandler(event) {
   event.preventDefault();
 
   const title = document.querySelector('input[name="post-title"]').value;
+  const contents = document.querySelector('textarea[name="content"]').value;
   const id = window.location.toString().split('/')[
     window.location.toString().split('/').length - 1
   ];
@@ -9,7 +10,8 @@ async function editFormHandler(event) {
   const response = await fetch(`/api/posts/${id}`, {
     method: 'PUT',
     body: JSON.stringify({
-      title
+      title,
+      contents
     }),
     headers: {
       'Content-Type': 'application/json'
