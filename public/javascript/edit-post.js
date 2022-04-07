88e2daf7c64e9430dcb6267807ec91a1c82083f2
nitrotap <kartikinpublic@gmail.com>
2022-04-06 21:24:13 -0600

    fixing edit post

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -1,26 +1,27 @@
 async function editFormHandler(event) {
-  event.preventDefault();
+	event.preventDefault();
 
-  const title = document.querySelector('input[name="post-title"]').value;
-  const id = window.location.toString().split('/')[
-    window.location.toString().split('/').length - 1
-  ];
+	const title = document.querySelector('input[name="post-title"]').value;
+	const id = window.location.toString().split('/')[
+		window.location.toString().split('/').length - 1
+	];
+	const contents = document.querySelector('textarea[name="content"]').value;
 
-  const response = await fetch(`/api/posts/${id}`, {
-    method: 'PUT',
-    body: JSON.stringify({
-      title
-    }),
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  });
+	const response = await fetch(`/api/posts/${id}`, {
+		method: 'PUT',
+		body: JSON.stringify({
+			title, contents
+		}),
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	});
 
-  if (response.ok) {
-    document.location.replace('/dashboard');
-  } else {
-    alert(response.statusText);
-  }
+	if (response.ok) {
+		document.location.replace('/dashboard');
+	} else {
+		alert(response.statusText);
+	}
 }
 
 document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
